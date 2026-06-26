95627cccf275091b86e1dcb5f59d9cb061ec73ef
jjtmutw <jjtmutw@gmail.com>
2026-06-26 15:47:59 +0800

    Add MQTT flip timer config

---
+++ flip_timer_config.js
@@ -0,0 +1,8 @@
+window.FLIP_TIMER_MQTT_CONFIG = {
+  DEFAULT_MQTT_HOST: "broker.emqx.io",
+  DEFAULT_MQTT_PORT: 1883,
+  DEFAULT_MQTT_USER: "",
+  DEFAULT_MQTT_PASS: "",
+  MQTT_WS_URL: "wss://broker.emqx.io:8084/mqtt",
+  TOPIC: "jj/ble/button/OY9LRGbg"
+};
