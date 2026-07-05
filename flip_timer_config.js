0394c230f97c8b1dbdd643c76617c84a45392088
jjtmutw <jjtmutw@gmail.com>
2026-07-05 19:16:09 +0800

    Add MQTT WebSocket fallback URLs

--- flip_timer_config.js
+++ flip_timer_config.js
@@ -4,5 +4,11 @@ window.FLIP_TIMER_MQTT_CONFIG = {
   DEFAULT_MQTT_USER: "",
   DEFAULT_MQTT_PASS: "",
   MQTT_WS_URL: "wss://broker.emqx.io:8084/mqtt",
+  MQTT_WS_URLS: [
+    "wss://broker.emqx.io:8084/mqtt",
+    "wss://broker.emqx.io:8084",
+    "ws://broker.emqx.io:8083/mqtt",
+    "ws://broker.emqx.io:8083"
+  ],
   TOPIC: "jj/ble/button/OY9LRGbg"
 };
